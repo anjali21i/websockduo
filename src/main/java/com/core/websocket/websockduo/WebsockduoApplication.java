9882bf9d82d7796bcc64d89464930c6f8b35216a
Anjali <anjaliguptajnp21@gmail.com>
2024-03-22 13:00:27 +0530

    Create WebsockduoApplication.java

---
+++ src/main/java/com/core/websocket/websockduo/WebsockduoApplication.java
@@ -0,0 +1,13 @@
+package com.core.websocket.websockduo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsockduoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsockduoApplication.class, args);
+	}
+
+}
