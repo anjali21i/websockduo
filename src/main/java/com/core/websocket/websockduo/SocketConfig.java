89b57108d266ed5911b985781c2eff3dd6d0e43a
Anjali Gupta <anjaliguptajnp21@gmail.com>
2024-03-23 23:21:12 +0530

    Socket config and beans
    Signed-off-by: Anjali Gupta <anjaliguptajnp21@gmail.com>

--- src/main/java/com/core/websocket/websockduo/SocketConfig.java
+++ src/main/java/com/core/websocket/websockduo/SocketConfig.java
@@ -2,22 +2,26 @@ package com.core.websocket.websockduo;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
+@EnableWebSocketMessageBroker
 public class SocketConfig implements WebSocketMessageBrokerConfigurer{
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        // TODO Auto-generated method stub
-        WebSocketMessageBrokerConfigurer.super.configureMessageBroker(registry);
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+        // WebSocketMessageBrokerConfigurer.super.configureMessageBroker(registry);
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        // TODO Auto-generated method stub
-        WebSocketMessageBrokerConfigurer.super.registerStompEndpoints(registry);
+        
+        registry.addEndpoint("/stomp-endpoint").withSockJS();
+        // WebSocketMessageBrokerConfigurer.super.registerStompEndpoints(registry);
     }
 
 
